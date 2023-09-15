f15a7a24e3124973af80cf700b3f0442c33eb9bb
Tiago Correa <tiagocsantana10@gmail.com>
2023-09-14 22:01:02 -0300

    [CreateApiTeste] feat: created componets to test API

---
+++ src/api/endpoints/TokenPost.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+function TokenPost() {
+  const [username, setUserName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [token, setToken] = React.useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(
+      'https://dogsapi.origamid.dev/json/jwt-auth/v1/token',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      },
+    );
+
+    console.log(response);
+
+    const data = await response.json();
+
+    setToken(data.token);
+
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset style={{ border: 'none' }}>
+        <input
+          type="text"
+          value={username}
+          placeholder="username"
+          onChange={({ target }) => setUserName(target.value)}
+        />
+      </fieldset>
+
+      <fieldset style={{ border: 'none' }}>
+        <input
+          type="text"
+          value={password}
+          placeholder="password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </fieldset>
+
+      <button>Enviar</button>
+      <p style={{ wordBreak: 'break-all' }}>{token}</p>
+    </form>
+  );
+}
+
+export default TokenPost;
