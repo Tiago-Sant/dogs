0dded387397f3296448835f5ba7b8b0bf3ea05b3
Tiago Correa <tiagocsantana10@gmail.com>
2023-09-22 21:06:14 -0300

    feat: stylized loginpage

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -4,6 +4,9 @@ import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import UseForm from '../../Hooks/UseForm';
 import { UserContext } from '../../UserContext';
+import Error from '../Helper/Error';
+import styles from './LoginForm.module.css';
+import stylesBtn from '../Forms/Button.module.css';
 
 function LoginForm() {
   const username = UseForm();
@@ -20,20 +23,30 @@ function LoginForm() {
   };
 
   return (
-    <section>
-      <h1>Login</h1>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
       <Link to="/login/create">Cadastro</Link>
 
-      <form onSubmit={handleSubmit}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" name="username" type="text" {...username} />
         <Input label="Senha" name="password" type="password" {...password} />
-        {error && <p className="error">{error}</p>}
+        {error && <Error error={error} />}
         {isLoading ? (
           <Button disabled>Carregando...</Button>
         ) : (
           <Button>Entrar</Button>
         )}
       </form>
+      <Link className={styles.forgot} to="/login/forgot">
+        Perdeu a Senha?
+      </Link>
+      <div className={styles.register}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to="/login/create">
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 }
